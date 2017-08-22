ba9b899ac74a31b7050eef258a3d9b9269c42605
“ABSteingit config --global user.email “abs19stein@gmail.com <andrewstein@Andrews-MacBook-Pro-2.local>
2017-08-21 21:27:51 -0500

    store

---
+++ store_item.rb
@@ -0,0 +1,12 @@
+store_item = {:product => "apple", :price => 1, :color => "red"}
+store_item_1 = {:product => "milk", :price => 2, :color => "white"}
+store_item_2 = {:product => "eggs", :price => 2, :color => "yellow"}
+
+
+
+
+
+
+p store_item_1[:product]
+p store_item_2[:color]  
+p store_item[:price]
